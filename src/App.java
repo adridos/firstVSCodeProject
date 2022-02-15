1b45378cb0446fe61ca353385c27c4aac52a9e90
Adriana Massey <adriana.dostine@hotmail.com>
2022-02-14 22:56:34 -0500

    I made a new method PracticeMethod

--- src/App.java
+++ src/App.java
@@ -2,5 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("I wish sout worked on vscode");
+        practiceMethod();
+    }
+    public static void practiceMethod(){
+        System.out.println("This is a practice method outside of main");
     }
 }
